75f9863795300c53833f3ea48981cc33eacffaa7
Nguyen Hoang Huy <nguyenhoanghuykma@gmail.com>
2020-04-12 10:56:28 +0700

    fixed font

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("menu").addEventListener("click", function() {
-  document.getElementById("display").classList.add("full-screen");
+  document.getElementById("full-screen").classList.add("full-screen");
   document.getElementsByClassName("show-icon").style.display = "inline-block";
 });
 function myFunction() {
@@ -7,8 +7,12 @@ function myFunction() {
   document
     .getElementById("product-dropdown")
     .classList.toggle("dropdown-rotate");
-  // document.getElementById("nav__product").style.display = "inline-block";
 }
+
+document.getElementById("full-screen").addEventListener("click", function() {
+  document.getElementById("navbarCollapse").classList.remove("show");
+  document.getElementById("full-screen").classList.remove("full-screen");
+});
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
@@ -34,13 +38,11 @@ $(document).ready(function() {
   var brand = $(".brand__list li");
   var ele = $("#carouselExampleIndicators .carousel-indicators li.active");
   if (ele) {
-    // console.log(ele.data("slide-to"));
     // $(".carousel-indicators-number li").addClass("active");
   }
 
   $win.on("click.Bts", function(e) {
     if ($win.has(e.target).length == 0 && !$nav.is(event.target)) {
-      // alert("outside");
     }
   });
   $(brand).click(function() {
@@ -48,43 +50,42 @@ $(document).ready(function() {
   });
 
   // carousel
-  $(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-      nav: true,
-      navText: ["<img src='./img/next.png'>", "<img src='./img/prev.png'>"],
-      loop: true,
-      dots: false,
-      margin: 33,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false
-        },
-        380: {
-          items: 1,
-          nav: false,
-          autoWidth: true
-        },
-        768: {
-          items: 2,
-          nav: true,
-          autoWidth: true,
-          margin: 30
-        },
-        1000: {
-          items: 3,
-          nav: true
-        },
-        1400: {
-          items: 4,
-          nav: true
-        },
-        1900: {
-          items: 5,
-          nav: true
-        }
+
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    navText: ["<img src='./img/next.png'>", "<img src='./img/prev.png'>"],
+    // loop: true,
+    dots: false,
+    margin: 33,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      380: {
+        items: 1,
+        nav: false,
+        autoWidth: true
+      },
+      768: {
+        items: 2,
+        nav: true,
+        autoWidth: true,
+        margin: 30
+      },
+      1000: {
+        items: 3,
+        nav: true
+      },
+      1400: {
+        items: 4,
+        nav: true
+      },
+      1900: {
+        items: 5,
+        nav: true
       }
-    });
+    }
   });
 });
